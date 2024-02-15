dba0e4d2caf0932aa1c513a6757b73b8e6a8f08c
Sarthak Rai <106958899+sarthak2685@users.noreply.github.com>
2024-02-15 22:16:38 +0530

    landing Component (#1)
    * landing page
    
    * landing-page
    
    ---------
    
    Co-authored-by: Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import girlImage from '../image/girl.png'; 
+import './banner.css';
+
+const Banner = () => {
+    return (
+        <div className="banner-container">
+             <div className="text-container">
+                <h1 className='h1'>
+                    <b>Empowering Excellence through<br /> Professional Growth and<br /> Lifelong Learning.</b>
+                </h1>
+                <p className='p'>Transform Your Career with Live Tech Bootcamps and<br/> Online Classes - Ignite Your Journey or Supercharge Your Success!</p>
+                <div className="button-container">
+                    <button className="button1">Get Started</button>
+                    <button className="button">Explore Courses</button>
+                </div>
+            </div>
+            <div className="image-container">
+                <img src={girlImage} alt="Girl" className="banner-image" />
+            </div>
+            <div className="gradient-ellipse"></div>
+            <div className="gradient-ellipse1"></div>
+        </div>
+    );
+}
+
+export default Banner;
