ccd71d365d4637d2b60c796bfeed290dbf80d981
Mridula Chowdhary <90252273+mridula-chowdhary@users.noreply.github.com>
2024-02-15 22:26:50 +0530

    Added sign up page (#4)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "./App.css";
+import Signup from "./Pages/Signup/Signup";
 
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
@@ -5,13 +7,16 @@ import HomePage from "./pages/HomePage";
 import About from './Components/About';
 function App() {
   return (
+
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
+          <Route path="/signup" element={<Signup/>}/>
       </Routes>
     </Router>
   );
+
 
 }
 
---
+++ src/Component/SignFb/SignFb.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './SignFb.css'
+import logo from '../../Assets/Social-logo/fb-logo.png'
+export default function SignFb() {
+  return (
+    <div>
+      <div className="fb w-72 text-md h-14 text-center rounded-xl flex justify-center items-center gap-2">
+      <img src={logo} alt='' className="h-6 w-6 cursor-pointer"></img>
+       <a href="/" className="text-black text-center">Sign in with Facebook</a>
+      </div>
+    </div>
+  )
+}
