80ee46f875b2d462782209da97b5a2a2b18d81f1
Sarthak Rai <106958899+sarthak2685@users.noreply.github.com>
2024-02-19 23:48:05 +0530

    Completed contact (#6)
    * contact-page
    
    * contact-page
    
    * contact-page
    
    * contact-page
    
    * resolved issues
    
    ---------
    
    Co-authored-by: ditya17 <nitya17nitya@gmail.com>

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from "./pages/About";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
+import Contact from "./pages/contacts/Contact";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/contact" element={<Contact />} />
         <Route path="/dujat" element={<CoursesChrist />} />
         <Route path="/login" element={<Login />} />
       </Routes>
