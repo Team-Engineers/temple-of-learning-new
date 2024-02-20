d0d8798601c678c0ae13d18065809486349436eb
sarthak <sarthaks2711btechcse2025@kccitm.edu.in>
2024-02-20 19:57:20 +0530

    rohtak-page

--- src/pages/Courses/rohtak/Rohtak.jsx
+++ src/pages/Courses/rohtak/Rohtak.jsx
@@ -9,7 +9,7 @@ const Rohtak = () => {
       <Navbar />
 
       <main id="body-content" className="main-content">
-
+      <div className="bg-[#C4F7D1] absolute inset-0 z-[-1] filter blur-[75px] w-56 h-48 border-black top-32 "></div>
         <section className="about-company-s2 section-padding">
           <div className="container-section">
             <div className="row">
@@ -19,12 +19,12 @@ const Rohtak = () => {
                 </h2>
               </div>
               <div className="col about-company-s2-slider-wrapper">
-                <p className="paragraph-justify"><b className='b'>COURSE(S) OFFERED: </b><b>Five Year Integrated Programme in Management (IPM)</b></p>
-                <p className="paragraph-justify"><b className='b'>NUMBER OF SEATS: </b><b>150</b></p>
+                <p className="paragraph-justify"><b className='b'>COURSE(S) OFFERED: </b><b className='b1'>Five Year Integrated Programme in Management (IPM)</b></p>
+                <p className="paragraph-justify"><b className='b'>NUMBER OF SEATS: </b><b className='b1'>150</b></p>
                 <p className="paragraph-justify"><b className='b'>ELIGIBILITY CRITERIA:</b></p>
-                <p className="paragraph-justify"><b className='b'>General, NC-OBC and EWS Category:</b><b> Minimum 60% in standard X/ SSC and standard XII/ HSC or equivalent examinations, Maximum 20 years of age as on July 31, 2020</b>.</p>
-                <p className="paragraph-justify"><b className='b'>SC, ST and PWD (DAP) Category:</b><b> Minimum 55% in standard X/ SSC and standard XII/ HSC or equivalent examinations, Maximum 20 years of age as on July 31, 2020.</b></p>
-                <p className="paragraph-justify"><b className='b'>SELECTION/ADMISSION PROCESS:</b><b>The candidates will be selected on the basis of overall performance in Aptitude Test (AT), Written Ability Test (WAT), Personal Interview (PI) & Past Academics. The final Merit List will be generated based on the composite score as mentioned below:</b></p>
+                <p className="paragraph-justify"><b className='b'>General, NC-OBC and EWS Category:</b><b className='b1'> Minimum 60% in standard X/ SSC and standard XII/ HSC or equivalent examinations, Maximum 20 years of age as on July 31, 2020</b>.</p>
+                <p className="paragraph-justify"><b className='b'>SC, ST and PWD (DAP) Category:</b><b className='b1'> Minimum 55% in standard X/ SSC and standard XII/ HSC or equivalent examinations, Maximum 20 years of age as on July 31, 2020.</b></p>
+                <p className="paragraph-justify"><b className='b'>SELECTION/ADMISSION PROCESS:</b><b className='b1'>The candidates will be selected on the basis of overall performance in Aptitude Test (AT), Written Ability Test (WAT), Personal Interview (PI) & Past Academics. The final Merit List will be generated based on the composite score as mentioned below:</b></p>
                 <div className="tab-pane fade show active" id="pills-home">
                   <table className="custom-table">
                     <thead className='component'>
@@ -84,16 +84,16 @@ const Rohtak = () => {
                     </tbody>
                   </table>
                 </div>
-                <p className="paragraph-justify"><b className='b'>NEGATIVE MARKING:</b> <b>YES</b></p>
-                <p className="paragraph-justify"><b className='b'>MARKING SCHEME:</b><b>+4 Correct answer & -1 for incorrect.</b></p>
-                <p className="paragraph-justify"><b className='b'>EXPECTED ENTRANCE DATE:</b><b>MAY 2<sup>nd</sup> Week</b></p>
+                <p className="paragraph-justify"><b className='b'>NEGATIVE MARKING:</b><b className='b1'>YES</b></p>
+                <p className="paragraph-justify"><b className='b'>MARKING SCHEME:</b><b className='b1'> +4 Correct answer & -1 for incorrect.</b></p>
+                <p className="paragraph-justify"><b className='b'>EXPECTED ENTRANCE DATE: </b><b className='b1'>MAY 2<sup>nd</sup> Week</b></p>
               </div>
             </div>
           </div>
         </section>
+        
         <div className='flex justify-center'>
-          <Link to="/login" className="relative bg-[#0D88CB] px-[80px] rounded-3xl p-2 flex text-center justify-center text-white text-[30px] items-center my-6 ">Register</Link>
-
+             <Link to="/login" className="register-button">Register</Link>
         </div>
       </main>
       <div>
