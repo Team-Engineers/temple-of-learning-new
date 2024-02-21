316ba102d7d0aa0ad310c2602a98121bac313c2e
Sarthak Rai <106958899+sarthak2685@users.noreply.github.com>
2024-02-21 12:31:37 +0530

    Feat jammu (#17)
    * Your commit message here
    
    * jammu-page
    
    ---------
    
    Co-authored-by: Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Cucet from "./pages/Courses/Cucet/Cucet";
 import DuJat from "./pages/Courses/DuJat/DuJat";
 import IIMRanchi from "./pages/Courses/IIM-Ranchi/IIMRanchi";
 import Indore from "./pages/Courses/indore/Indore";
+import Jammu from "./pages/Courses/jammu/Jammu";
 import Rohtak from "./pages/Courses/rohtak/Rohtak";
 import HomePage from "./pages/HomePage";
 import Login from "./pages/Login/Login";
@@ -28,6 +29,7 @@ function App() {
         <Route path="/iim-rohtak" element={<Rohtak />} />
         <Route path="/iim-ranchi" element={<IIMRanchi />} />
         <Route path="/iim-indore" element={<Indore />} />
+        <Route path="/iim-jammu" element={<Jammu/>} />
       </Routes>
     </Router>
   );
