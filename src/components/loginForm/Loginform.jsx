3aafb9e2736f99b08b224176f80ed977d1029aa0
Mridula Chowdhary <90252273+mridula-chowdhary@users.noreply.github.com>
2024-02-21 11:18:41 +0530

    Modification in signup login (#14)
    * Added sign up page
    
    * login page changes
    
    * change in path
    
    * Added some margin in bullets and change the routes of cucet
    
    * Made some changes in margin
    
    * Modification in login and signup images
    
    * Changes in login and signup
    
    * modified login signup
    
    ---------
    
    Co-authored-by: ditya17 <nitya17nitya@gmail.com>
    Co-authored-by: Nitya Dwivedi <96773479+ditya17@users.noreply.github.com>

--- src/components/loginForm/Loginform.jsx
+++ src/components/loginForm/Loginform.jsx
@@ -5,23 +5,18 @@ const Loginform = () => {
   return (
     <div className="container mx-auto py-8">
       <form className="max-w-lg mx-auto bg-white  rounded  pt-6 pb-8 mb-4">
-        {/* <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="full-name">
-          </label>
-          <input className=" border-b border-gray-500  w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline " id="full-name" type="text" placeholder="Your Full Name" />
-        </div> */}
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="">
           </label>
-          <input className="  border-b border-gray-500 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Your Email Address" />
+          <input className="  border-b border-gray-500 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline" type="email" placeholder="Your Email Address" />
         </div>
         <div className="mb-6">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
           </label>
-          <input className="  border-b border-gray-500 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Password" />
+          <input className="  border-b border-gray-500 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="password" placeholder="Password" />
         </div>
         <div className="mb-6">
-          <button className="bg-[#77D3FF] w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline shadow-md rounded-lg" id="btn"> Create Account </button>
+          <button className="bg-[#77D3FF] w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline shadow-md rounded-lg"> Create Account </button>
         </div>
         <div className="mb-4">
           <p> Already have an account? <a href="/signup" className="text-[#77D3FF]" >Signup</a></p>
