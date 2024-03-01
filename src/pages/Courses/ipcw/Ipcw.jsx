dc7bf2213faed5a782e08b7e5b5d2e6be192c3e2
Sarthak Rai <106958899+sarthak2685@users.noreply.github.com>
2024-03-01 12:33:15 +0530

    Feat ipcw (#24)
    * Your commit message here
    
    * ipcw-page

---
+++ src/pages/Courses/ipcw/Ipcw.jsx
@@ -0,0 +1,236 @@
+import { Link } from 'react-router-dom';
+import Navbar from '../../../components/Navbar/Navbar';
+import Footer from '../../../components/footer/Footer';
+// import './Ipcw.css';
+
+
+const IPCW = () => {
+  return (
+    <div>
+      <Navbar />
+
+      <main id="body-content" className="main-content max-w-[1200px] mx-auto overflow-hidden">
+        <div className="bg-[#C4F7D1] absolute inset-0 z-[-1] filter blur-[75px] w-56 h-48 border-black top-32 "></div>
+        <section className="about-company-s2 section-padding">
+          <div className="container-section">
+            <div className="row">
+              <div className="col-sm-12 wow fadeInDown" data-wow-duration="0" data-wow-delay="0s">
+                <h2 className="heading-main-containers">
+                IPCW University
+                </h2>
+              </div>
+              <div className="col about-company-s2-slider-wrapper">
+                <p className="ph-justify"><b className='b-container'>B.A. (H) Multimedia and Mass Communication (BMMC)</b> is a self-financed UGC approved course run only by I.P College under the University of Delhi. The Admission to this course is through Entrance Exam</p>
+                <p className="ph-justify"><b className='b-container'>Eligibility:</b></p>
+                <ul style={{ listStyleType: 'disc', marginLeft: '1.5rem' }}>
+                    <li><p className="ph-justify">General : 75% aggregate of best of four including 85% in English</p></li>
+                    <li><p className="ph-justify">OBC/CW/KM : 70 % aggregate of best of four including 70% in English</p></li>
+                    <li><p className="ph-justify">OBC/CW/KM : 70 % aggregate of best of four including 70% in English</p></li>
+                </ul>
+
+                <p className="ph-justify"><b className='b-container'>Selection : MCQ ONLINE Entrance</b></p>
+                <p className="ph-justify">The test is on General Awareness, Media Awareness, Current Affairs, English Comprehension, Grammatical and Analytical Skills and will be a multiple choice question test of 2 hours duration.</p>
+
+                <p className="ph-justify"><b className='b-container'>Test Structure:</b></p>
+                <ul style={{ listStyleType: 'disc', marginLeft: '1.5rem' }}>
+                    <li><p className="ph-justify">The Entrance test is of Multiple Choice Question (MCQ) format. There will be 100 questions. For each correct answer, a student shall score +4 marks and for each wrong answer, there will be -1 mark.</p></li>
+                    <li><p className="ph-justify">OThere will be no descriptive questions.</p></li>
+                </ul>
+                <p className="ph-justify"><b className='b-container'>Seats :</b> Total 52 Seats</p>
+                <p className="ph-justify"><b className='b-container'>Fee Structure:</b> Rs 75,000 per year</p>
+                <p className="ph-justify"><b className='b-container'>COURSE CURRICULUM:</b></p>
+
+
+                <div className="tab-pane fade show active" id="pills-home">
+                  <table className="custom-table1">
+                    <thead className='component1'>
+                      <tr>
+                        <th>SEMESTER</th>
+                        <th>CORE
+                        COURSE (14)</th>
+                        <th>ABILITY
+                         ENHANCEMENT
+                         COMPULSORY
+                         COURSE
+                        (AECC) (2)</th>
+                        <th>ABILITY
+                        ENHANCEMENT
+                        ELECTIVE
+                        COURSE
+                        (AEEC) (2)
+                        (SKILL BASED)</th>
+                        <th>ELECTIVE
+                        DISCIPLIN-E
+                        SPECIFIC
+                        DSE (4)</th>
+                        <th>ELECTIVE
+                        GENERIC
+                        (GE) (4)</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>I</td>
+                        <td>C MMMC 01 Communication, Media and Society
+                        C MMMC 02 Introduction to Journalism</td>
+                        <td>English Communication</td>
+                        <td></td>
+                        <td></td>
+                        <td>GE 1 Advertisement and Media</td>
+                        </tr>
+                      <tr>
+                      <td>II</td>
+                        <td>C MMMC 03 Communication Research
+                        C MMMC 04 Print Media Production</td>
+                        <td>Environmental Science</td>
+                        <td></td>
+                        <td></td>
+                        <td>GE 2 Social Media</td>
+                      </tr>
+                      <tr>
+                      <td>III</td>
+                        <td>C MMMC05 Exploring Hindi Cinema	</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                      </tr>
+                      <tr>
+                      <td></td>
+                        <td>C MMMC 06</td>
+                        <td></td>
+                        <td>AEEC-1</td>
+                        <td></td>
+                        <td>GE 3</td>
+
+                      </tr>
+                      <tr>
+                      <td></td>
+                        <td>Graphic Designing &Visual Images</td>
+                        <td></td>
+                        <td>Theatre and Communication</td>
+                        <td></td>
+                        <td>Public Service Broadcasting</td>
+
+                      </tr>
+                      <tr>
+                      <td></td>
+                        <td>CMMMC 07</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                      </tr>
+                      <tr>
+                      <td></td>
+                        <td>Television Journalism</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                      </tr>
+                      <tr>
+                      <td>IV</td>
+                        <td>C MMMC 08 Development Communication	</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                      </tr>
+                      <tr>
+                      <td></td>
+                        <td>C MMMC 09</td>
+                        <td></td>
+                        <td>AEEC-2</td>
+                        <td></td>
+                        <td>GE 4</td>
+
+                      </tr>
+                      <tr>
+                      <td></td>
+                        <td>Camera and Editing for TV</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>Communication & Disaster Management</td>
+
+                      </tr>
+                      <tr>
+                      <td></td>
+                        <td>C MMMC 10</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                      </tr>
+                      <tr>
+                      <td></td>
+                        <td>Reporting and Anchoring	</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                      </tr>
+                      <tr>
+                      <td>V</td>
+                        <td>C MMMC 11 Global Politics and Media	</td>
+                        <td></td>
+                        <td></td>
+                        <td>DSE1 Radio Production</td>
+                        <td></td>
+
+                      </tr>
+                      <tr>
+                      <td></td>
+                        <td>C MMMC 12 Documentary</td>
+                        <td></td>
+                        <td></td>
+                        <td>DSE 2 Photography</td>
+                        <td></td>
+
+                      </tr>
+                      <tr>
+                      <td>VI</td>
+                        <td>C MMMC 13 Communicatio n and the Plastic Arts</td>
+                        <td></td>
+                        <td></td>
+                        <td>DSE 3 Communicatio n for Special Needs</td>
+                        <td></td>
+
+                      </tr>
+                      <tr>
+                      <td></td>
+                        <td>CMMMC14 Integrated Marketing Communicatio n	</td>
+                        <td></td>
+                        <td></td>
+                        <td>DSE 4 Fashion Communication	</td>
+                        <td></td>
+
+                      </tr>
+                      
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <div className='flex justify-center'>
+          <Link to="/login" className="register-button1">Register</Link>
+        </div>
+      </main>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default IPCW;
